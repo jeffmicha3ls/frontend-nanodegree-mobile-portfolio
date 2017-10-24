c259ff0411ffd9f6881d3b1e4782e38dd57af87e
Jeff Michaels <jeff@themichaelsfamily.com>
2017-10-23 21:59:25 -0700

    Changed to getElementsByClassName

--- views/js/main.js
+++ views/js/main.js
@@ -437,7 +437,7 @@ changeSliderLabel(size);
           console.log("bug in sizeSwitcher");
       }
 
-    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    var randomPizzas = document.getElementsByClassName("randomPizzaContainer");
     var pizzaLength = randomPizzas.length;
     for (var i = 0; i < pizzaLength; i++) {
       randomPizzas[i].style.width = newWidth + "%";
@@ -492,13 +492,12 @@ function updatePositions() {
 
   // document.body.scrollTop is no longer supported in Chrome.
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  //var items = document.querySelectorAll('.mover');
   var items = document.getElementsByClassName('mover');
   var numberOfItems = items.length;
   for (var i = 0; i < numberOfItems; i++) {
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.transform = 'translate3D('+ (items[i].basicLeft + 100 * phase) + 'px, 0px, 0px)';
-    //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    //items[i].style.transform = 'translate3D('+ (items[i].basicLeft + 100 * phase) + 'px, 0px, 0px)';
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -518,7 +517,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0, elem; i < 24; i++) {
+  for (var i = 0, elem; i < 55; i++) {
     elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/dest/pizza.png";
@@ -528,5 +527,5 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.getElementById("movingPizzas1").appendChild(elem);
   }
-  //updatePositions();
+  updatePositions();
 });
